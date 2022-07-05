53f31fce4b002a35efbefecef93d2728b314ae63
David Zucker <davzucky@hotmail.com>
2022-07-05 08:34:26 +0800

    Fix documentation after enabling pre-commit

--- prefect_opentelemetry/monitors/monitor.py
+++ prefect_opentelemetry/monitors/monitor.py
@@ -1,13 +1,39 @@
+"""
+Defines the monitor protocol
+"""
 from typing import Protocol
-from fastapi import FastAPI
-from opentelemetry.trace import TracerProvider
+
 from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+from opentelemetry.trace import TracerProvider
+
 
 class Monitor(Protocol):
+    """
+    Protocol that define a Monitor that allow to enable OpenTelemetry instrumentation
+
+    Args:
+        instrumentor: BaseIntrumentor
+
+    """
 
     @property
     def instrumentor(self) -> BaseInstrumentor:
+        """
+        Property that return the baseInstrumentor
+
+        Returns:
+            Returns the BaseInstrumentor
+
+        """
         pass
 
     def monitor(self, trace_provider: TracerProvider) -> None:
+        """
+        Enable the instrumentation of FastAPI
+
+        Args:
+            tracer_provider: Allow to initialize the TracerProvider to set default
+                resources
+
+        """
         pass
--- tests/monitors/test_fastapi_monitor.py
+++ tests/monitors/test_fastapi_monitor.py
@@ -1,9 +1,9 @@
 import pytest
-from fastapi import FastAPI
-from prefect_opentelemetry.monitors import fastapi_monitoring
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+
+from prefect_opentelemetry.monitors import fastapi_monitoring
 
 
 @pytest.mark.parametrize(
@@ -27,8 +27,7 @@ def test_call_fast_api_instrumentor_once_with_app(
     instrumentor = FastAPIInstrumentor()
     spy = mocker.spy(instrumentor, "instrument")
 
-    app = FastAPI()
-    monitor = fastapi_monitoring.FastAPIMonitoring(instrumentor=instrumentor)
+    monitor = fastapi_monitoring.FastAPIMonitor(instrumentor=instrumentor)
     monitor.monitor(tracer_provider=tracer_provider)
 
     assert spy.call_count == 1
