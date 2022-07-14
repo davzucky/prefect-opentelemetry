a7c9ad631a522d7cc30a8c8c81d89d7b480a58d8
David Zucker <davzucky@hotmail.com>
2022-07-14 09:01:11 +0900

    Add monitor into the init of monitors module

--- prefect_opentelemetry/monitors/__init__.py
+++ prefect_opentelemetry/monitors/__init__.py
@@ -1,6 +1,8 @@
+from .monitor import Monitor
 from .otlp_monitors import FastAPIMonitor, SQLLite3Monitor, SQLAlchemyMonitor
 
 __all__ = [
+    "Monitor",
     "FastAPIMonitor",
     "SQLLite3Monitor",
     "SQLAlchemyMonitor",
